10cd53521a94ccaa75e0e69cfb97e80e8f2e3f73
slw <offgridauthor@gmail.com>
2023-08-25 20:40:55 -0500

    update chunk size

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,6 @@ export default defineConfig({
     base: '/bananas/',
     build: {
         chunkSizeWarningLimit: 1600,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
     }
+
 })
